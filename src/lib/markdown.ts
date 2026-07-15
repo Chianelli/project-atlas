7662d19aeb7f224470550eb606b4ddb03048e091
Chianelli <105399776+Chianelli@users.noreply.github.com>
2026-07-15 14:59:54 -0300

    Update markdown.ts

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,1 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
+const contentDirectory = path.join(process.cwd(), 'content');
+
+export function getLesson(slugArray: string[]) {
+  const fullPath = path.join(contentDirectory, ...slugArray) + '.md';
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const { data, content } = matter(fileContents);
+  return { metadata: data, content };
+}
