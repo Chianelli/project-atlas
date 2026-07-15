44ffd93015e7aa042362d6adcb9a4e0a46a198b0
Chianelli <105399776+Chianelli@users.noreply.github.com>
2026-07-15 14:58:57 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        atlas: {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          600: '#2563eb', // Azul Oficial Atlas
+          700: '#1d4ed8',
+          900: '#1e3a8a',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
